7459f3bb764269d6e902dbbce7f8a3fa3f3c6a01
Waian Serano <serano1u@etu.univ-lorraine.fr>
2020-09-18 15:27:45 +0200

    DAO Architecture

---
+++ src/DAOFactory.java
@@ -0,0 +1,25 @@
+package src.view;
+
+public abstract class DAOFactory {
+    
+    public static DAOFactory getDAOFactory(Persistance cible) {
+        DAOFactory daoF = null;
+        switch (cible) {
+        case MYSQL:
+        daoF = new MySQLDAOFactory();
+        break;
+        case ListeMemoire:
+        daoF = new ListeMemoireDAOFactory();
+        break;
+        }
+        return daoF;
+        }
+        public abstract EtudiantDAO getEtudiantDAO();
+        public abstract PromoDAO getPromoDAO();
+       
+}
+
+
+
+
+
---
+++ src/Persistance.java
@@ -0,0 +1,5 @@
+package src.view;
+
+public enum Persistance {
+    MYSQL, LISTE_MEMOIRE
+}
