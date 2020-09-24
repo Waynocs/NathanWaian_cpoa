593581ff4a082731aad10d47a471baf017bf2b0b
Waynocs <64638869+Waynocs@users.noreply.github.com>
2020-09-24 14:18:27 +0200

    Finished Command

--- src/model/Command.java
+++ src/model/Command.java
@@ -12,9 +12,43 @@ public class Command {
 
     private class Line {
 
-        public Product product;
+        public int idProduct;
         public double cost;
         public int quantity;
+
+        public Line(int idProduct, double cost, int quantity) {
+            this.setIdProduct(idProduct);
+            this.setCost(cost);
+            this.setQuantity(quantity);
+        }
+
+        public int getIdProduct() {
+            return idProduct;
+        }
+
+        public void setIdProduct(int idProduct) {
+            if (idProduct > 0)
+                this.idProduct = idProduct;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public void setCost(double cost) {
+            if (cost > 0)
+                this.cost = cost;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            if (quantity > 0)
+                this.quantity = quantity;
+
+        }
 
     }
 }
