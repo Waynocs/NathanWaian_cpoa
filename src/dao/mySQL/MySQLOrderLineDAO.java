325226e0ce43f55e1c742f653c6f45103232ff0a
Nathan Hanen <hanen4u@etu.univ-lorraine.fr>
2020-09-25 10:56:09 +0200

    MySQLOrderLineDAO theorically done

--- src/dao/mySQL/MySQLOrderLineDAO.java
+++ src/dao/mySQL/MySQLOrderLineDAO.java
@@ -1,20 +1,26 @@
 package dao.mySQL;
 
 import java.sql.SQLException;
+import java.util.LinkedList;
 
 import dao.OrderLineDAO;
 import model.OrderLine;
 
+/**
+ * OrderLineDAO with the SQL mode
+ */
 public class MySQLOrderLineDAO implements OrderLineDAO {
 
-    // Instance of the class
     private static MySQLOrderLineDAO instance;
 
-    // constructor
     private MySQLOrderLineDAO() {
     }
 
-    // Insure to get only 1 instance
+    /**
+     * Returns the only instance of this class
+     * 
+     * @return the only instance of this class
+     */
     public static MySQLOrderLineDAO getInstance() {
         if (instance == null)
             instance = new MySQLOrderLineDAO();
@@ -24,37 +30,63 @@ public class MySQLOrderLineDAO implements OrderLineDAO {
 
     @Override
     public boolean create(OrderLine object) throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
+        var statement = Request.Connection.getConnection().createStatement();
+        return statement.executeUpdate(
+                "INSERT INTO `Ligne_commande`(`id_commande`, `id_produit`, `quantite`, `tarif_unitaire`) VALUES ("
+                        + object.getOrder() + ", " + +object.getProduct() + ", " + +object.getQuantity() + ", "
+                        + +object.getCost() + ")") != 0;
     }
 
     @Override
     public boolean update(OrderLine object) throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
+        var statement = Request.Connection.getConnection().createStatement();
+        return statement.executeUpdate("UPDATE `Ligne_commande` SET `quantite`=" + object.getQuantity()
+                + ",`tarif_unitaire`=" + object.getCost() + " WHERE `id_produit`=" + object.getProduct()
+                + " AND `id_commande`=" + object.getOrder()) != 0;
     }
 
     @Override
     public boolean delete(OrderLine object) throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
+        var statement = Request.Connection.getConnection().createStatement();
+        return statement.executeUpdate("DELETE FROM `Ligne_commande` WHERE `id_produit`=" + object.getProduct()
+                + " AND `id_commande`=" + object.getOrder()) != 0;
     }
 
     @Override
     public OrderLine getById(int order, int product) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        var statement = Request.Connection.getConnection().createStatement();
+        var result = statement.executeQuery(
+                "SELECT `id_produit`, `id_commande`, `quantite`, `tarif_unitaire` FROM `Ligne_commande` WHERE"
+                        + "`id_produit`=" + product + " AND `id_commande`=" + order);
+        return result.next()
+                ? new OrderLine(result.getInt("id_commande"), result.getInt("id_produit"),
+                        result.getDouble("tarif_unitaire"), result.getInt("quantite"))
+                : null;
     }
 
     @Override
     public OrderLine[] getAllFromOrder(int order) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        var statement = Request.Connection.getConnection().createStatement();
+        var result = statement.executeQuery(
+                "SELECT `id_produit`, `id_commande`, `quantite`, `tarif_unitaire` FROM `Ligne_commande` WHERE"
+                        + "`id_commande`=" + order);
+        var list = new LinkedList<OrderLine>();
+        while (result.next())
+            list.add(new OrderLine(result.getInt("id_commande"), result.getInt("id_produit"),
+                    result.getDouble("tarif_unitaire"), result.getInt("quantite")));
+        return list.toArray(new OrderLine[0]);
     }
 
     @Override
     public OrderLine[] getAllFromProduct(int product) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        var statement = Request.Connection.getConnection().createStatement();
+        var result = statement.executeQuery(
+                "SELECT `id_produit`, `id_commande`, `quantite`, `tarif_unitaire` FROM `Ligne_commande` WHERE"
+                        + "`id_produit`=" + product);
+        var list = new LinkedList<OrderLine>();
+        while (result.next())
+            list.add(new OrderLine(result.getInt("id_commande"), result.getInt("id_produit"),
+                    result.getDouble("tarif_unitaire"), result.getInt("quantite")));
+        return list.toArray(new OrderLine[0]);
     }
 }
