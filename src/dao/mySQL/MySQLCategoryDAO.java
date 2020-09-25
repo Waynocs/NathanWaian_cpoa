e5f1b5a715d19e385defff17ee8b71f5876501e4
Waynocs <64638869+Waynocs@users.noreply.github.com>
2020-09-25 21:13:04 +0200

    Added all functions except getAll()

--- src/dao/mySQL/MySQLCategoryDAO.java
+++ src/dao/mySQL/MySQLCategoryDAO.java
@@ -25,21 +25,34 @@ public class MySQLCategoryDAO implements CategoryDAO {
 
     // executer une requete SQL
     public boolean create(Category object) throws SQLException {
-        return false;
+        var statement = Request.Connection.getConnection()
+                .prepareStatement("INSERT INTO `categorie`(`id_categorie`, `titre`, `visuel`) VALUES (" + object.getId()
+                        + ", " + object.getName() + ", " + object.getImagePath());
+
+        return statement.executeUpdate() != 0;
     }
 
     public boolean update(Category object) throws SQLException {
-        return false;
+        var statement = Request.Connection.getConnection().prepareStatement("UPDATE `categorie` SET `id_categorie`= "
+                + object.getId() + ",`titre`= " + object.getName() + ",`visuel`= " + object.getImagePath());
+
+        return statement.executeUpdate() != 0;
     }
 
     public boolean delete(Category object) throws SQLException {
-        return false;
+        var statement = Request.Connection.getConnection()
+                .prepareStatement("DELETE FROM `categorie` WHERE `id_categorie` = " + object.getId());
+
+        return statement.executeUpdate() != 0;
     }
 
     // method pr recuperer depuis la bdd un item
     public Category getById(int id) throws SQLException {
-
-        return null;
+        var statement = Request.Connection.getConnection().createStatement();
+        var result = statement.executeQuery("SELECT `id_categorie`, `titre`, `visuel`=" + id);
+        return result.next()
+                ? new Category(result.getString("title"), result.getString("visuel"), result.getInt("id_categorie"))
+                : null;
     }
 
     public Category[] getAll() throws SQLException {
