6af17d97e1d1588c281246320cbf9a7b5afbfdcf
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-09-26 17:37:02 +0200

    added some utility functions

---
+++ src/controller/Utilities.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.util.Scanner;
+
+public class Utilities {
+    private Utilities() {
+    }
+
+    private static Scanner consoleInput;
+
+    /**
+     * Returns the one and only scanner that should be used to read the console
+     * 
+     * @return the one and only scanner that should be used to read the console
+     */
+    public static Scanner getConsoleInput() {
+        return consoleInput == null ? consoleInput = new Scanner(System.in) : consoleInput;
+    }
+
+    /**
+     * Returns the number typed by the user, with the minimal value set to 1
+     * 
+     * @param message  message to be displayed on the screen
+     * @param maxValue maximal value (inclusive)
+     * @return the number typed by the user
+     */
+    public static int getUserSelection(String message, int maxValue) {
+        return getUserSelection(message, 1, maxValue);
+    }
+
+    /**
+     * Returns the number typed by the user
+     * 
+     * @param message  message to be displayed on the screen
+     * @param minValue minimal value (inclusive)
+     * @param maxValue maximal value (inclusive)
+     * @return the number typed by the user
+     */
+    public static int getUserSelection(String message, int minValue, int maxValue) {
+        int input = minValue - 1;
+        var console = getConsoleInput();
+        do {
+            System.out.println(message);
+            if (console.hasNextInt()) {
+                input = console.nextInt();
+                if (input < minValue || maxValue < input)
+                    System.out.println("[ERROR] You must type a valid number");
+            } else
+                System.out.println("[ERROR] You must type a number");
+            console.nextLine(); // free the buffer
+        } while (minValue > input || input > maxValue);
+        return input;
+    }
+}
