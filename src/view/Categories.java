da174615630b6db2f0f6006a11f7e9ff8d9b1250
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-09-29 19:01:09 +0200

    view : can see a specific category

--- src/view/Categories.java
+++ src/view/Categories.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import dao.DAOFactory;
 import model.Category;
@@ -28,6 +30,9 @@ public class Categories {
                 case 1:
                     seeAll(factory);
                     break;
+                case 2:
+                    seeItem(factory);
+                    break;
                 case 3:
                     newItem(factory);
                     break;
@@ -41,12 +46,48 @@ public class Categories {
         Utilities.displayList(categories, (categ) -> categ.getId() + ":" + categ.getName());
     }
 
+    private static void seeItem(DAOFactory factory) {
+        int input = -1;
+        System.out.print(Utilities.getSeparator());
+        do {
+            System.out.println("Enter the id of the category to display");
+            if (Utilities.getConsoleInput().hasNextInt()) {
+                input = Utilities.getConsoleInput().nextInt();
+                if (input < 0) {
+                    System.out.println("Enter a number higher than zero :");
+                    input = -1;
+                }
+            } else
+                System.out.println("Enter a number :");
+            Utilities.getConsoleInput().nextLine();
+        } while (input == -1);
+        var categ = factory.getCategoryDAO().getById(input);
+        if (categ == null) {
+            System.out.println("This category doesn't exist.");
+            return;
+        }
+        System.out.println(Utilities.getSeparator());
+        System.out.print(Utilities.mapToString(getMap(categ, true)));
+        System.out.println("\n>>>Press Enter to go back to menu---");
+        Utilities.getConsoleInput().nextLine();
+    }
+
+    private static Map<Object, Object> getMap(Category category, boolean useID) {
+        var details = new HashMap<Object, Object>();
+        if (useID)
+            details.put("ID", category.getId());
+        details.put("Name", category.getName());
+        details.put("Visual", category.getImagePath());
+        return details;
+    }
+
     private static void newItem(DAOFactory factory) {
         var tmpItem = new Category("name", "file.jpg", 0);
         int input;
         do {
             System.out.println(Utilities.getSeparator());
-            input = Utilities.getUserSelection("1. Edit name\n2. Edit image file\n3. Cancel\n4.Add", 4);
+            input = Utilities.getUserSelection(Utilities.mapToString(getMap(tmpItem, false))
+                    + "────────────\n1. Edit name\n2. Edit image file\n3. Cancel\n4.Add", 4);
             if (input == 1) {
                 System.out.print("Enter the new name :\n>");
                 tmpItem.setName(Utilities.getConsoleInput().nextLine());
@@ -56,6 +97,7 @@ public class Categories {
             }
         } while (input < 3);
         if (input == 4)
-            factory.getCategoryDAO().create(tmpItem);
+            if (!factory.getCategoryDAO().create(tmpItem))
+                System.out.println("[ERROR] Unable to create the item");
     }
 }
