f3da3a08023d30e554daa61b367f7c7c3493e22b
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-09-30 10:15:29 +0200

    view : finished category management

--- src/view/Categories.java
+++ src/view/Categories.java
@@ -36,6 +36,12 @@ public class Categories {
                 case 3:
                     newItem(factory);
                     break;
+                case 4:
+                    editItem(factory);
+                    break;
+                case 5:
+                    removeItem(factory);
+                    break;
             }
         } while (input != 6);
 
@@ -44,6 +50,29 @@ public class Categories {
     private static void seeAll(DAOFactory factory) {
         Category[] categories = factory.getCategoryDAO().getAll();
         Utilities.displayList(categories, (categ) -> categ.getId() + ":" + categ.getName());
+    }
+
+    private static void removeItem(DAOFactory factory) {
+        int input = -1;
+        System.out.print(Utilities.getSeparator());
+        do {
+            System.out.println("Enter the id of the category to display");
+            if (Utilities.getConsoleInput().hasNextInt()) {
+                input = Utilities.getConsoleInput().nextInt();
+                if (input < 0) {
+                    System.out.println("Enter a number higher than zero :");
+                    input = -1;
+                }
+            } else
+                System.out.println("Enter a number :");
+            Utilities.getConsoleInput().nextLine();
+        } while (input == -1);
+        var categ = factory.getCategoryDAO().getById(input);
+        if (categ == null) {
+            System.out.println("This category doesn't exist.");
+            return;
+        }
+        factory.getCategoryDAO().delete(categ);
     }
 
     private static void seeItem(DAOFactory factory) {
@@ -81,13 +110,51 @@ public class Categories {
         return details;
     }
 
+    private static void editItem(DAOFactory factory) {
+
+        int input = -1;
+        System.out.print(Utilities.getSeparator());
+        do {
+            System.out.println("Enter the id of the category to display");
+            if (Utilities.getConsoleInput().hasNextInt()) {
+                input = Utilities.getConsoleInput().nextInt();
+                if (input < 0) {
+                    System.out.println("Enter a number higher than zero :");
+                    input = -1;
+                }
+            } else
+                System.out.println("Enter a number :");
+            Utilities.getConsoleInput().nextLine();
+        } while (input == -1);
+        var categ = factory.getCategoryDAO().getById(input);
+        if (categ == null) {
+            System.out.println("This category doesn't exist.");
+            return;
+        }
+        do {
+            System.out.println(Utilities.getSeparator());
+            input = Utilities.getUserSelection(Utilities.mapToString(getMap(categ, false))
+                    + "────────────\n1. Edit name\n2. Edit image file\n3. Cancel\n4. Finish", 4);
+            if (input == 1) {
+                System.out.print("Enter the new name :\n>");
+                categ.setName(Utilities.getConsoleInput().nextLine());
+            } else if (input == 2) {
+                System.out.print("Enter the new image file :\n>");
+                categ.setImagePath(Utilities.getConsoleInput().nextLine());
+            }
+        } while (input < 3);
+        if (input == 4)
+            if (!factory.getCategoryDAO().update(categ))
+                System.out.println("[ERROR] Unable to create the item");
+    }
+
     private static void newItem(DAOFactory factory) {
         var tmpItem = new Category("name", "file.jpg", 0);
         int input;
         do {
             System.out.println(Utilities.getSeparator());
             input = Utilities.getUserSelection(Utilities.mapToString(getMap(tmpItem, false))
-                    + "────────────\n1. Edit name\n2. Edit image file\n3. Cancel\n4.Add", 4);
+                    + "────────────\n1. Edit name\n2. Edit image file\n3. Cancel\n4. Add", 4);
             if (input == 1) {
                 System.out.print("Enter the new name :\n>");
                 tmpItem.setName(Utilities.getConsoleInput().nextLine());
