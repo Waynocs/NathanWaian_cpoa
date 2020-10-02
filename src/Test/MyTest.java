3ba3a864eeb93c0ffa01a9a243d70851851b9c64
Waian Serano <serano1u@etu.univ-lorraine.fr>
2020-10-02 09:15:44 +0200

    first test for JUnit

---
+++ src/Test/MyTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
