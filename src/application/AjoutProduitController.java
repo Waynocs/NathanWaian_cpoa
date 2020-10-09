ba4fefb11e01f4fbf0674bfdc0740ebb496c0fbd
Nathan Hanen <hanen4u@etu.univ-lorraine.fr>
2020-10-09 14:44:18 +0200

    auto load categories

--- src/application/AjoutProduitController.java
+++ src/application/AjoutProduitController.java
@@ -1,15 +1,32 @@
 package application;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AjoutProduitController {
+import dao.DAOFactory;
+import dao.DAOFactory.Mode;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import model.Category;
+
+public class AjoutProduitController implements Initializable {
 
     @FXML
     private Label lblAffichage;
+    @FXML
+    private ChoiceBox<Category> cboxCateg;
 
     public void creerProduit() {
         this.lblAffichage.setText("Bonjour");
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        var factory = DAOFactory.getFactory(Mode.MEMORY);
+        cboxCateg.setItems(FXCollections.observableArrayList(factory.getCategoryDAO().getAll()));
     }
 
 }
