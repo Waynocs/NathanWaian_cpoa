496f39f589c15852f91d12d7b346a9c22dbe65a9
Waynocs <64638869+Waynocs@users.noreply.github.com>
2020-10-13 19:03:30 +0200

    added errors messages for added product

--- src/application/AjoutProduitController.java
+++ src/application/AjoutProduitController.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import model.Category;
 
 public class AjoutProduitController implements Initializable {
@@ -17,16 +19,54 @@ public class AjoutProduitController implements Initializable {
     @FXML
     private Label lblAffichage;
     @FXML
+    private Label lblNom;
+    @FXML
+    private Label lblDesc;
+    @FXML
+    private Label lblTarif;
+    @FXML
     private ChoiceBox<Category> cboxCateg;
+    @FXML
+    private TextField editNom;
+    @FXML
+    private TextArea editDesc;
+    @FXML
+    private TextField editTarif;
 
     public void creerProduit() {
-        this.lblAffichage.setText("Bonjour");
+        String name = editNom.getText().trim();
+        String desc = editDesc.getText().trim();
+        float cost = 0;
+        try {
+            cost = Float.parseFloat(editTarif.getText().trim());
+        } catch (NumberFormatException e) {
+
+        }
+
+        if (name.isEmpty()) {
+            String errorName = "Nom non saisie ou non valide";
+            this.lblNom.setText(errorName);
+        }
+        if (desc.isEmpty()) {
+            String errorDesc = "Description non saisie";
+            this.lblDesc.setText(errorDesc);
+        }
+        if (cost <= 0) {
+            String errorCost = "Tarif non saisie ou non valide";
+            this.lblTarif.setText(errorCost);
+        }
+
     }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         var factory = DAOFactory.getFactory(Mode.MEMORY);
         cboxCateg.setItems(FXCollections.observableArrayList(factory.getCategoryDAO().getAll()));
+
+    }
+
+    public String toString() {
+        return this.editNom + " " + this.editDesc + this.editTarif;
     }
 
 }
