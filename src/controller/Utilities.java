9c7f406fc8a6aeb6e4242f376257c63a5d9b78db
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-10-20 22:02:34 +0200

    fixed search algorithm

--- src/controller/Utilities.java
+++ src/controller/Utilities.java
@@ -9,10 +9,10 @@ public class Utilities {
     public static boolean compareStrings(String key, String value) {
         if (key.length() == 0)
             return true;
-        key = Normalizer.normalize(key.toLowerCase().replace(" ", "").replaceAll("[^\\p{ASCII}]", ""),
-                Normalizer.Form.NFD);
-        value = Normalizer.normalize(value.toLowerCase().replace(" ", "").replaceAll("[^\\p{ASCII}]", ""),
-                Normalizer.Form.NFD);
+        key = Normalizer.normalize(key.toLowerCase().replace(" ", ""), Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]",
+                "");
+        value = Normalizer.normalize(value.toLowerCase().replace(" ", ""), Normalizer.Form.NFD)
+                .replaceAll("[^\\p{ASCII}]", "");
         return value.contains(key);
     }
 }
