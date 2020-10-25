f7149cb7981be1e8f79a7a01f9bf906c7a690f1e
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-10-25 18:10:36 +0100

    removed Main warnings, no more console UI

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import view.MainMenu;
-import view.Utilities;
 import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
@@ -37,7 +35,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        // MainMenu.start();
         launch(args);
     }
 }
